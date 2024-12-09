0658a872ea87826f86dfc9f82586863ecc81871a
Tsk-Andrey078 <tskandrey2002.@gmail.com>
2024-12-09 15:35:42 +0500

    Cors

--- abu_cheta/abu_api/admin.py
+++ abu_cheta/abu_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Participant, Criterios, Scores, CustomUser
 
 # Register your models here.
+admin.site.register(Participant)
+admin.site.register(Criterios)
+admin.site.register(Scores)
+admin.site.register(CustomUser)
